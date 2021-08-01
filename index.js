a3c6b62eb5c3676615550b3710c03663cb172507
Yuyuhita Guzman <yuyuhita.guzman@gmail.com>
2021-08-01 19:18:57 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, message){
+  const newArr = [];
+  for(let i=0; i < names.length; i++){
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+    debugger;
+  }
+  return newArr;
+}
+
+function countDown(number){
+  while (number >= 0) {
+    console.log(number); number--; 
+  }
+}
+
